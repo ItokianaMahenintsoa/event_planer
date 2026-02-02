fb6b8bd59aa1198ddee8092112118df6f7c24e18
fy.mahenintsoa <mahenintsoafyitokiana@gmail.com>
2026-02-02 10:32:30 +0300

    Add Events Root

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from routes.users import user_router
+from routes.events import event_router
+
 import uvicorn
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
 
 #A another way to run the app
