76ca182a83a4e776ab7e13d32862b9070d52553c
fy.mahenintsoa <mahenintsoafyitokiana@gmail.com>
2026-02-25 10:08:54 +0300

    Add sign in test

--- tests/test_login.py
+++ tests/test_login.py
@@ -22,3 +22,17 @@ async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
     assert response.status_code == 200
     assert response.json() == test_response
     
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_sign_user_in(default_client: httpx.AsyncClient) -> None:
+    payload = {
+        "email": "testuser@packt.com",
+        "password": "testpassword",
+    } 
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    response = await default_client.post("/user/signin", json=payload, headers=headers)
+    assert response.status_code == 200
+    assert response.json()["token_type"].lower() == "bearer"
